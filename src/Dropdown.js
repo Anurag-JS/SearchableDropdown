824076fcabfccb275fadcbcff3185c1343701516
Anurag <anuragv8269@gmail.com>
2024-09-18 17:46:46 +0530

    Added isLoading and Error states

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,28 +1,40 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export default function Dropdown (){
 
     const [data, setData] = useState(null);
-    const [isLoading, setIs]
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(()=>{
         const fetchData = async () => {
             try{
                 let response = await fetch("https://jsonplaceholder.typicode.com/users");
                 let result = await response.json();
+                setData(result);
                 console.log(result);
             }catch(err){
+                setError(err);
                 console.log("Error",err)
+            }finally{
+                setIsLoading(false);
             }
         }
         fetchData()
     },[])
 
+    if(isLoading){
+        return <p><h2>Loading...</h2></p>
+    }
+
+    if(error){
+        return <p><h2>{`Error - ${error}`}</h2></p>
+    }
+
     return (
         <div>
-            
-            haka
+            {String(data)}
         </div>
     )
 }
